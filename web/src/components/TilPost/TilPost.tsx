b63276d57be6fb92e7f7d26138d7cde2a752eaa7
Manassarn Manoonchai <manassarn@gmail.com>
2022-01-31 00:10:34 +0700

    Show link only if authenticated

--- web/src/components/TilPost/TilPost.tsx
+++ web/src/components/TilPost/TilPost.tsx
@@ -1,16 +1,21 @@
+import { useAuth } from '@redwoodjs/auth'
 import { Link, routes } from '@redwoodjs/router'
 import { Post } from 'types/graphql'
 
-const TilPost = ({ post }: Post) => {
+const TilPost = ({ post }: { post: Post }) => {
+  const { isAuthenticated } = useAuth()
+
   return (
     <div className="p-4 bg-gray-300 rounded mt-4">
       <h2>{post.title}</h2>
       <p>{post.content}</p>
-      <p>
-        <Link to={routes.post({ id: post.id })} className="underline">
-          Read
-        </Link>
-      </p>
+      {isAuthenticated && (
+        <p>
+          <Link to={routes.post({ id: post.id })} className="underline">
+            View
+          </Link>
+        </p>
+      )}
     </div>
   )
 }
